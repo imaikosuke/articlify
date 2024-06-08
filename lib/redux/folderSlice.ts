d79dfc22cfe57ac348b1045f5a2f01c7a5fe6bc6
kou7306 <kouta.1206.y@icloud.com>
2024-06-09 01:21:51 +0900

    temp

--- lib/redux/folderSlice.ts
+++ lib/redux/folderSlice.ts
@@ -5,14 +5,18 @@ export const folderSlice = createSlice({
   name: "folder",
   initialState: {
     folderId: "", // Update the initial state to an empty string
+    folders: [],
   },
   reducers: {
     // フォルダ名を設定するリデューサー
     setFolderId: (state, action: PayloadAction<string>) => {
       state.folderId = action.payload; // action.payloadを参照する
     },
+    setFolders: (state, action) => {
+      state.folders = action.payload;
+    },
   },
 });
 
-export const { setFolderId } = folderSlice.actions;
+export const { setFolderId, setFolders } = folderSlice.actions;
 export default folderSlice.reducer;
