50fb8f84b688ab2fd1a7874817ff1e402826cfda
kou7306 <kouta.1206.y@icloud.com>
2024-06-09 03:41:54 +0900

    user-auth

---
+++ app/api/user_auth/route.ts
@@ -0,0 +1,42 @@
+import axios from "axios";
+import { NextResponse } from "next/server";
+import { db } from "../../../lib/firebase/FirebaseConfig";
+import {
+  getDocs,
+  getDoc,
+  collection,
+  doc,
+  deleteDoc,
+  query,
+  where,
+} from "firebase/firestore";
+
+export async function POST(req: Request) {
+  const body = await req.json();
+
+  try {
+    // ドキュメントの参照を取得
+    const q = query(collection(db, "Users"), where("uid", "==", body.uid));
+    const querySnapshot = await getDocs(q);
+    if (!querySnapshot.empty) {
+      const docSnapshot = querySnapshot.docs[0];
+      console.log("Document data:", docSnapshot.data());
+      return NextResponse.json({
+        data: docSnapshot.data(),
+      });
+    } else {
+      return NextResponse.json(
+        {
+          error: "Document not found",
+        },
+        { status: 404 }
+      );
+    }
+  } catch (error) {
+    // エラーレスポンスを返す
+    return NextResponse.json(
+      { error: "Failed to fetch user data" },
+      { status: 500 }
+    );
+  }
+}
