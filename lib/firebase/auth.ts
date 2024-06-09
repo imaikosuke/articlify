b7c8a8eb36be09ea7602a937bfad0cef7e052c0d
kou7306 <kouta.1206.y@icloud.com>
2024-06-09 14:40:24 +0900

    redirect

--- lib/firebase/auth.ts
+++ lib/firebase/auth.ts
@@ -1,14 +1,30 @@
 import { signInWithPopup } from "firebase/auth";
-import { auth, googleProvider } from "./FirebaseConfig";
+import { auth, googleProvider, db } from "./FirebaseConfig";
 import Cookies from "js-cookie";
+import {
+  getDocs,
+  getDoc,
+  collection,
+  doc,
+  deleteDoc,
+  updateDoc,
+  query,
+  where,
+  addDoc,
+} from "firebase/firestore";
 
 export const signInWithGoogle = async () => {
-  signInWithPopup(auth, googleProvider)
-    .then((result) => {
-      const user = result.user.uid;
-      Cookies.set("user", user);
-    })
-    .catch((error) => {
-      console.log(error);
+  try {
+    const result = await signInWithPopup(auth, googleProvider);
+    const user = result.user.uid;
+    const docRef = await addDoc(collection(db, "Users"), {
+      uid: user,
     });
+
+    Cookies.set("user", user);
+
+    return user;
+  } catch (error) {
+    console.log(error);
+  }
 };
